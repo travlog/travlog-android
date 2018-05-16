79d32a40bac94181c1105ef4783eac65c936a338
fobidlim <fobidlim@gmail.com>
2018-05-16 22:48:21 +0900

    Convert ApiEndpoint to kotlin

---
+++ app/src/main/java/com/travlog/android/apps/libs/ApiEndpoint.kt
@@ -0,0 +1,30 @@
+package com.travlog.android.apps.libs
+
+enum class ApiEndpoint(private val type: String, private var url: String) {
+    PRODUCTION("Production", ""),
+    STAGING("Staging", ""),
+    LOCAL("Local", "http://192.168.0.5:3000/api/"),
+    CUSTOM("Custom", "");
+
+    fun url(): String {
+        return this.url
+    }
+
+    override fun toString(): String {
+        return this.type
+    }
+
+    companion object {
+
+        fun from(url: String): ApiEndpoint {
+            for (value in values()) {
+                if (value.url == url) {
+                    return value
+                }
+            }
+            val endpoint = CUSTOM
+            endpoint.url = url
+            return endpoint
+        }
+    }
+}
