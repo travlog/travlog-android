7e1bf4d9a47809fa4b90d73d59bb6af374640c8f
fobidlim <fobidlim@gmail.com>
2019-05-27 17:06:26 +0900

    Update copyright

--- app/src/main/java/com/travlog/android/apps/libs/perferences/StringPreference.kt
+++ app/src/main/java/com/travlog/android/apps/libs/perferences/StringPreference.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Jake Wharton
+ * Copyright 2019 Travlog. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,11 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * ***
- *
- * Original: https://github.com/JakeWharton/u2020/blob/7363d27ee0356e24dcbd00dc6926d993ee56d6e2/src/main/java/com/jakewharton/u2020/data/prefs/StringPreference.java
- * Modifications: Some modifiers and annotations have been added by Kickstarter.
  */
 
 package com.travlog.android.apps.libs.perferences
